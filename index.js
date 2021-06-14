6c9389caae16133eeafc3ce7569f948bae6919d7
James Allsop <james.allsop8@gmail.com>
2021-06-14 18:58:12 +0200

    startup change

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const mainRouter = require('./mainRoutes')
 
-app.use(mainRouter)
-
 const port = process.env.PORT || 3000
 app.listen(port)
 console.log('Express server running on port', port)
+
+app.use(mainRouter)
