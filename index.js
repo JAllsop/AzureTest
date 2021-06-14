1c6f03405fa808282a6fc7bfa5fcc3390b129afd
James Allsop <james.allsop8@gmail.com>
2021-06-14 19:50:42 +0200

    not using express

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const app = express()
-const mainRouter = require('./mainRoutes')
+'use strict'
 
-const port = process.env.PORT || 3000
-app.listen(port)
-console.log('Express server running on port', port)
-
-app.use(mainRouter)
+const http = require('http');
+const port=process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+res.end('<h1>Hello World</h1>');
+});
+server.listen(port,() => {
+console.log(`Server running at port `+port);
+});
