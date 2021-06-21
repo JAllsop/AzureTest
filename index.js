6441e1938349af289c90832a8e8cfff5da93d09e
James Allsop <james.allsop8@gmail.com>
2021-06-21 10:38:50 +0200

    add jest and travis

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
-const mainRouter = require('./mainRoutes')
 
-const port = process.env.PORT || 3000
-app.listen(port)
-console.log('Express server running on port', port)
+// loading body-parser
+const bodyParser = require('body-parser')
 
-app.use(mainRouter)
+// loading our routers
+const mainRouter = require('./mainRoutes.js')
+const classRouter = require('./classRoutes.js')
+
+// tell Express to use bodyParser for JSON and URL encoded form bodies
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// mounting our routers
+app.use('/', mainRouter)
+app.use('/class', classRouter)
+app.listen(3000)
+console.log('Express server running on port 3000')
