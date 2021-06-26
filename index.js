19d1d0088c3e13ece7e31f06fab81a3bf9759095
James Allsop <james.allsop8@gmail.com>
2021-06-26 19:55:36 +0200

    enabled port fetching

--- index.js
+++ index.js
@@ -26,5 +26,6 @@ app.get('/', (req, res) => {
 
 // mounting router
 app.use('/class', classRouter)
-app.listen(3000)
+const port = process.env.PORT || 3000
+app.listen(port)
 console.log('Express server running on port 3000')
